c7de17950105d26461d702f94dc7b54308adc74e
Vinay sagar <vrs223@gmail.com>
2021-02-28 15:00:08 +0530

    Level 1 Username and Password only

--- app.js
+++ app.js
@@ -10,16 +10,51 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/secretsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const secretSchema = new mongoose.Schema({
-
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
 });
 
-const Secret = mongoose.model("Secret", secretSchema);
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
-  res.render('home')
+  res.render('home');
+});
+
+app.get("/register", function(req, res){
+  res.render('register');
+});
+
+app.get("/login", function(req, res){
+  res.render('login');
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+  User.findOne({username: req.body.username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser.password == req.body.password){
+        res.render("secrets");
+      }
+    }
+  })
 })
 
 

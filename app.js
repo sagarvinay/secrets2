9179f0adb31471411c362e1e4c2ae2cc5c6cae15
Vinay sagar <vrs223@gmail.com>
2021-03-01 13:49:16 +0530

    Level 3 - Hashing with md5

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const ejs = require("ejs");
+const md5 = require("md5");
 
 const app = express();
-console.log();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,9 +19,6 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']} );
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +37,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -57,7 +53,7 @@ app.post("/login", function(req, res){
     if(err){
       console.log(err);
     } else {
-      if(foundUser.password == req.body.password){
+      if(foundUser.password == md5(req.body.password)){
         res.render("secrets");
       }
     }
